d6cee0f4cfb39a36e7ebd93683fef66bbe89efbd
InsertData711 <qa9975@ync.ac.kr>
2025-11-11 10:05:13 +0900

    fix: test파일 수정  -  tests/integration/example/test_health.py: asyncio가 없어서 파일 수정  - .env.example: dev_db와 test_db로 분리
    #6: 대응 중

--- tests/integration/example/test_health.py
+++ tests/integration/example/test_health.py
@@ -1,10 +1,18 @@
 import pytest
-import httpx
-import asyncio
+import asyncpg
 
 pytestmark = pytest.mark.integration
 
 @pytest.mark.asyncio
-async def test_health_endpoint():
-  async with httpx.AsyncClient() as clint:
-    assert 1 == 1
+async def test_db_connection():
+  conn = await asyncpg.connect(
+    user = 'master',
+    password = 'test_password',
+    database = 'test_db',
+    host = 'db',
+    port = 5432
+  )
+  
+  result = await conn.fetchval("select 1;")
+  await conn.close()
+  assert result == 1
